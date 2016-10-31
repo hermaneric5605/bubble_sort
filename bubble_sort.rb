e05eb0b8dfd569c2ade3ab014fea0c3b04a83294
Eric Herman <hermaneric@outlook.com>
2016-10-31 11:06:36 -0400

    finished buble_sort

---
+++ bubble_sort.rb
@@ -0,0 +1,22 @@
+def bubble_sort(array)
+	n = array.length
+
+	loop do 
+	# [1, 4, 1, 3, 4, 1, 3, 3]
+		
+		swapped = false
+
+		(n-1).times do |i|
+			if array[i] > array[i + 1]
+				array[i], array[i + 1] = array[i +1], array[i]
+			end		
+		end
+
+		break if not swapped
+	end
+
+	array
+end
+
+a = [1, 4, 1, 3, 4, 1, 3, 3]
+puts bubble_sort(a)
